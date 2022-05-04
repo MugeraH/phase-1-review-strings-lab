1d717b8c3d34bf5dd631942ed30ffe66b54ea24a
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-05-04 14:18:34 +0300

    commit tree fe9cdb1b084b4f7d77f1d79632a1e44ba479f865

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+let currentUser = "Mello";
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage =
+  "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, ";
+
+shortGreeting = "Welcome, " + currentUser[0] + "!";
